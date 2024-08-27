f909c29117d90124f5fae49bd6b8d4f36aa07683
Lzu_Elias <lzuecmoreira@hotmail.com>
2024-08-27 15:05:21 -0300

    Teste 3

--- teste_2_SBrT_Vicotria.py
+++ teste_2_SBrT_Vicotria.py
@@ -18,22 +18,20 @@ import matplotlib.pyplot as plt
 import random
 
 
-K = 10                                 # N° dispositivos
+K = 10                                   # N° dispositivos
 N = 4                                   # N° de antenas
-R = 10                                  # Raio [m]
+R = 50                                  # Raio [m]
 f = 915 *(10**6)                        # Freq. da portadora
 kappa = 1.5                             # Fator Rice
 mu = 10.73 * 10**(-3)                   # Max pot no IoT
 a = 0.2308                              # constante circuito a
 b = 5.365                               # constante circuito b  
-Pt = 3                                  # Max pot. de transmissão
+Pt = 2                                  # Max pot. de transmissão
 E_min = 200 * (10**-6)                  # Min Energia no IoT [J]
 alpha = 2.7                             # Coef. perda de percurso
 c = 3*(10**(8))                         # Vel. da luz
-Omega = 1/(1+np.exp(a*b))               # Constante resposta 0_in-0_out
+Omega = 1/(1+(np.exp(a*b)))             # Constante resposta 0_in-0_out
 Pot_k = 0
-tau_k_estrela_k_igual_1_valor = 0
-tau_k_valor = 0
 
 
 def dist_euclid_quad (x, y):
@@ -76,43 +74,45 @@ def canal_h_k (kappa, h_barra_k, h_til_k): # Gerador de canal
 
 #%%
 #Rodando
-canal_h = np.array([])
-h_barra = np.array([])
-h_til = np.array([])
-Beta = np.array([])
-Pot = np.array([])
-Psi_k_estrela = np.array([])
+canal_h = np.zeros((K,N)).astype(complex)
+h_barra = np.zeros((K,N)).astype(complex)
+h_til = np.zeros((K,N)).astype(complex)
+Beta = np.zeros(K)
+Pot = np.zeros(K)
+Psi_k_estrela = np.zeros((K,N)).astype(complex)
 Gamma = np.array([])
 Gamma_k_j = np.array([])
 Phi_NEIG_j = np.array([])
-tau_k_estrela = np.array([])
+tau_k_estrela = np.zeros([K])
 tau = np.array([])
 tau_total = np.array([])
-
 # H = np.zeros((K,N))
 Phi_NEIG_j_vetor = np.array([])
-
+h = np.zeros((N,K)).astype(complex)
+h_k = np.zeros((N,K)).astype(complex)
 seed = np.random.seed(9)
 
-k=0
-for k in range (0, K):
-    # Canal h
-    
-    h_barra = h_barra_k(N) # LoS (1x4)
-    h_til = h_til_k(N)     # NLoS (1x4) 
-    canal_h = canal_h_k(kappa, h_barra, h_til) # (1x4)
-    h_k = np.conjugate(np.transpose(canal_h)) # Hermitiano (4x1)
-    
+# Canal h
+h_barra = np.random.rand(K,N) + 1j*np.random.rand(K,N)
+h_til = np.random.rand(K,N) + 1j*np.random.rand(K,N)
+canal_h = (np.sqrt((kappa)/(1+kappa)) * h_barra) + (np.sqrt((1)/(1+kappa)) * h_til)
+h_k = np.conjugate(np.transpose(canal_h)) # Hermitiano (4x1)
+h = h_k
+
+for k in range (0,K):
     # Beta
     x =  np.random.randint(1,10)
     y =  np.random.randint(1,10)
-    Beta = Beta_k(dist_euclid_quad(x,y)) # (Constante)
-   
+    Beta[k] = Beta_k(dist_euclid_quad(x,y)) # (Constante)
+
     # Beamforming S-CSI
-    Psi_k_estrela = (np.sqrt(Pt/N)) *(h_barra/np.abs(h_barra)) #(1x4)
-    
+    Psi_k_estrela[k][:] = (np.sqrt(Pt/N)) *(h_barra[:][k]/np.abs(h_barra[:][k])) #(1x4)
+
+
+for k in range (0, K):
+  
     # Potência   
-    Pot = np.append(Pot, Beta*(np.abs(Psi_k_estrela @ h_k))**2) # array(10)
+    Pot[k] = (Beta[k]*(np.abs(Psi_k_estrela[k][:].dot(h[:][k])))**2) # array(10)
 
     # Gamma (função logística tradicional)
     # Gamma = Gamma_k(Pot) # array(10)  
@@ -120,19 +120,28 @@ for k in range (0, K):
     Gamma = mu/(1 + (np.exp(aux_1)))
     
     # Tau_k para k=0
+    
     if k == 0:
         tau_0 = (E_min*(1-Omega)) / (Gamma[0] - (mu*Omega)) # Valor fixo
-        tau_k_estrela = np.append(tau_k_estrela, tau_0) # Vetor para calcular Phi_NEIG (array(10))
+        # tau_k_estrela = np.append(tau_k_estrela, tau_0) # Vetor para calcular Phi_NEIG (array(10))
+        tau_k_estrela[0] = tau_0
     else:
-        produto_escalar = np.abs(Psi_k_estrela @ h_k)**2 # Norma (Psi * H_hermitiano) **2
-        Gamma_k_j = mu / (1+(np.exp(-a*((Beta*produto_escalar)-b)))) # Valor
-        Phi_NEIG_j = (tau_k_estrela[k-1]) * ((Gamma_k_j - (mu*Omega))/(1-Omega))
-        Phi_NEIG_j_vetor = np.append(Phi_NEIG_j_vetor, Phi_NEIG_j)
-        Phi_NEIG_j_sum = np.sum(Phi_NEIG_j_vetor)
-        tau = ((E_min - Phi_NEIG_j_sum) * (1-Omega)) / (Gamma[k] - (mu*Omega))
-        # if tau < 0: tau = 0
-        tau_k_estrela = np.append(tau_k_estrela, tau)
+        for j in range (0,k):
+            # print(j)
+            produto_escalar = np.abs(Psi_k_estrela[j][:] @ h[:][k])**2 # Absolut (Psi * H_hermitiano) **2
+            Gamma_k_j = mu / (1+(np.exp(-a*((Beta[k]*produto_escalar)-b)))) # Valor
+            Phi_NEIG_j = (tau_k_estrela[j]) * ((Gamma_k_j - (mu*Omega))/(1-Omega))
+            Phi_NEIG_j_vetor = np.append(Phi_NEIG_j_vetor, Phi_NEIG_j)
+            Phi_NEIG_j_sum = np.sum(Phi_NEIG_j_vetor)
+            if(Phi_NEIG_j_sum > E_min): 
+                tau=0
+            else:
+                tau = ((E_min - Phi_NEIG_j_sum) * (1-Omega)) / (Gamma[k] - (mu*Omega))
+                # if tau < 0: tau = 0
+            tau_k_estrela[k] = tau
+    # print(f"k = {k}")
 tau_total = np.append(tau_total, sum(tau_k_estrela))
+
 
 
 
